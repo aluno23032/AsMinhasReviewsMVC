3cc1582c85d99900e811c9c8f10063af4e450d47
aluno23032 <aluno23032@ipt.pt>
2022-05-09 17:01:08 +0100

    Adição da classe "Fotografias"

---
+++ Models/Fotografias.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SiteReviews.Models
+{
+    public class Fotografias
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey(nameof(IdUtilizador))]
+        public int UtilizadorFK { get; set; }
+        public Utilizadores IdUtilizador { get; set; }
+
+        public string Fotografia { get; set; }
+    }
+}
