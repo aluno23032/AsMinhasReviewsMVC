ea7c5ff1d8acd8cfb17b034eb0aab02aea1abcd2
Eduardo Gomes <aluno23032@ipt.pt>
2022-09-19 23:19:00 +0100

    Página detalhes

---
+++ Controllers/DetalhesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+namespace AsMinhasReviews.Controllers
+{
+    public class DetalhesController : Controller
+    {
+        private readonly ILogger<DetalhesController> _logger;
+        public DetalhesController(ILogger<DetalhesController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
